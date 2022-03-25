ec6200159cb6541fb53e20ca6ef9ab17bc6f5349
ackermanjayjay <reza23198@gmail.com>
2022-03-25 20:18:54 +0700

    There a bug in function fungsi minimum price cannot show

--- ProgramKasir.py
+++ ProgramKasir.py
@@ -1,12 +1,26 @@
 class kasir:
-    totalmkn,porsi,gelas,totalsemua,totalmkn,totalmnm,uang=0
-    mkn,mnm=""
+
+    ## Tipe data Number
+    totalmk=0,
+    porsi=0
+    gelas=0
+    totalsemua=0
+    totalmkn=0
+    totalmnm=0
+    uang=0
+    nomor=0
+    kembalian=0
+
+    ## String
+    mkn=""
+    mnm=""
+    pembeli=""
 
  
     def tampilan(self):
         print("------------------------------- Program Kasir Sederhana Samet -------------------")
-        pembeli = input("Masukkan Nama pembeli: ")
-        print("Nama Pembeli :", pembeli)
+        self.pembeli = input("Masukkan Nama pembeli: ")
+        print("Nama Pembeli :", self.pembeli)
 
     #Fungsi Makan
     def fungsimakanan(self):
@@ -17,20 +31,20 @@ class kasir:
         print("1. Mie Ayam Bakso - Rp 15000")
         print("2. Soto Mie - Rp 20000")
         print("3. Nasi uduk spesial - Rp 13000")
-        nomor= int(input("Masukkan Pilihan: "))
-        porsi= int(input("Berapa Porsi: "))
+        self.nomor= int(input("Masukkan Pilihan: "))
+        self.porsi= int(input("Berapa Porsi: "))
 
-        if nomor==1:
-            self.totalmkn=porsi*15000
-            print (porsi, "Porsi Nasi Goreng Telur = Rp{} ",format(self.totalmkn))
+        if self.nomor==1:
+            self.totalmkn=self.porsi*15000
+            print (self.porsi, "Porsi Nasi Goreng Telur = Rp  ",format(self.totalmkn))
             self.mkn="Mie Ayam Bakso"
-        elif nomor==2:
-            self.totalmkn=porsi*20000
-            print (porsi, "Porsi Soto Mie = Rp{}", format(self.totalmkn))
+        elif self.nomor==2:
+            self.totalmkn=self.porsi*20000
+            print (self.porsi, "Porsi Soto Mie = Rp", format(self.totalmkn))
             self.mkn="Soto Mie"
-        elif nomor==3:
-            self.totalmkn=porsi*13000
-            print (porsi, " Porsi Nasi uduk spesial = Rp{}", format(self.totalmkn))
+        elif self.nomor==3:
+            self.totalmkn=self.porsi*13000
+            print (self.porsi, " Porsi Nasi uduk spesial = Rp", format(self.totalmkn))
             self.mkn="Nasi Uduk Spesial"
         else:
             print("Pilihan Tidak ada, Silahkan Masukan lagi!!")
@@ -43,52 +57,63 @@ class kasir:
         print("1. Es Teh Manis - Rp 5000")
         print("2. Jeruk Anget - Rp 6000")
         print("3. Kopi Espresso - Rp 15000")
-        nomor=int(input("Masukkan Pilihan: "))
-        gelas=int(input("Berapa Gelas :"))
+        
+        self.nomor=int(input("Masukkan Pilihan: "))
+        self.totalsemuagelas=int(input("Berapa Gelas :"))
 
-        if nomor==1:
-            totalmnm=gelas*5000
-            print (gelas, " Gelas Es Teh Manis = Rp", totalmnm)
+        if self.nomor==1:
+            self.totalmnm=self.gelas*5000
+            print (self.gelas, " Gelas Es Teh Manis = Rp", self.totalmnm)
             self.mnm=(" Gelas Es Teh Manis")
-        elif nomor==2:
-            totalmnm=gelas*6000
-            print (gelas, "Gelas Jeruk Anget = Rp", totalmnm)
+
+        elif self.nomor==2:
+            self.totalmnm=self.gelas*6000
+            print (self.gelas, "Gelas Jeruk Anget = Rp", self.totalmnm)
             self.mnm=("Gelas Es Jeruk")
-        elif nomor==3:
-            totalmnm=gelas*15000
-            print (gelas, "Gelas Kopi ESpresso = Rp", totalmnm)
+
+        elif self. nomor==3:
+            self.totalmnm=self.gelas*15000
+            print (self.gelas, "Gelas Kopi ESpresso = Rp", self.totalmnm)
             self.mnm=("Gelas Kopi Espresso")
+
         else:
             print("Pilihan Tidak ada, Silahkan Masukan lagi!!")
             self.fungsiminuman()
     
 
     def total(self):
-        totalsemua=self.totalmkn+self.totalmnm
-        print("\nTotal Harus Dibayar: Rp", totalsemua)
-        self.fuang=int(input("Uang Tunai Pembeli: Rp "))
-        kembalian=int(self.uang-self.totalsemua)
-        print("Kembalian :", kembalian)
+        self.totalsemua=self.totalmkn+self.totalmnm
+        print("\nTotal Harus Dibayar: Rp", self.totalsemua)
+        try:
+            self.uang=int(input("Uang Tunai Pembeli: Rp "))
+            self.kembalian=self.uang-self.totalsemua
+            print("Kembalian :", self.kembalian)
+        except:
+                if self.uang<self.totalsemua:
+                    print("Uang anda tidak cukup")
 
     def output(self):
     #Pencetakkan User Input yang memanggil value yang telah diinputkan sebelumnya
         print("\n========================================")
         print("=================== S T R U K   B E L I ======================")
         print("==============================================================")
-        print("Nama\t\t:",pembeli)
-        print("Beli\t\t:",porsi,mkn,"(Rp", self.totalmkn, ")")
-        print("\t\t ",self.gelas,mnm,"( Rp", self.totalmnm, ")")
+        print("Nama\t\t:",self.pembeli)
+        print("Beli\t\t:",self.porsi,self.mkn,"(Rp", self.totalmkn, ")")
+        print("\t\t ",self.gelas,self.mnm,"( Rp", self.totalmnm, ")")
         print("Tagihan\t\t: Rp", self.totalsemua)
         print("Dibayar\t\t: Rp", self.uang)
         print("Kembalian\t: Rp", self.kembalian)
         print("===============================================================")
         print("===============================================================")
 
+## Call object
+cetak=kasir()
 
-    
-    #Manggil fungsi
-    fungsimakanan()
-    fungsiminuman()
-
+#Manggil fungsi
+cetak.tampilan()
+cetak.fungsimakanan()
+cetak.fungsiminuman()
+cetak.total()
+cetak.output()
 
 
